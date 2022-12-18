d43a4c72941bedfc31eb744f76eafe096c6c1727
Ivan Golubev <g0lubew.iv@yandex.ru>
2022-12-18 18:25:52 +0300

    The last commit...

--- cat-n-mouse.cpp
+++ cat-n-mouse.cpp
@@ -18,48 +18,52 @@ struct Mouse {
 
 
 int main() {
-    Cat tom;
-    tom.r = {10, 0};
-    tom.v = {0, 1.145};
-    tom.attack = 0;
+    int y_m(0), x_c(0);
+    std::cin >> y_m >> x_c;
+    double v_res = Norm(Rdec2D{x_c, 0} - Rdec2D{0, y_m});
+    for (int v_ = 0.1; v_ += 0.1; v_ <= v_res) {
+        Cat tom;
+        tom.r = {x_c, 0};
+        tom.v = {0, v_};
+        tom.attack = 0;
 
-    Mouse jerry;
-    jerry.r = {0, -10};
-    jerry.v = {0, 1};
+        Mouse jerry;
+        jerry.r = {0, y_m};
+        jerry.v = {0, 1};
 
-    Rdec2D last_r   = tom.r;
-    Rdec2D last_r_m = jerry.r;
-    bool is_catched = false;
-    bool is_safe    = false;
-    double s_mouse  = 0;
-    double s_cat    = 0;
-    while (!is_catched && !is_safe) {
-        last_r   = tom.r;
-        last_r_m = jerry.r;
-        tom.v    = (jerry.r - tom.r) * (1 / getnorm(jerry.r - tom.r)) * getnorm(tom.v);
-        tom.r    += tom.v * dt;
-        if (getnorm(tom.v * dt) >= getnorm(jerry.r - last_r)) {
-            is_catched = true;
-            s_cat     += getnorm(jerry.r - last_r);
-            s_mouse   += getnorm(jerry.r - last_r);
-            tom.r      = jerry.r;
+        Rdec2D last_r   = tom.r;
+        Rdec2D last_r_m = jerry.r;
+        bool is_catched = false;
+        bool is_safe    = false;
+        double s_mouse  = 0;
+        double s_cat    = 0;
+
+        while (!is_catched && !is_safe) {
+            last_r   = tom.r;
+            last_r_m = jerry.r;
+            tom.v    = (jerry.r - tom.r) * (1 / Norm(jerry.r - tom.r)) * Norm(tom.v);
+            tom.r    += tom.v * dt;
+            if (Norm(tom.v * dt) >= Norm(jerry.r - last_r)) {
+                is_catched = true;
+                s_cat   += Norm(jerry.r - last_r);
+                s_mouse += Norm(jerry.r - last_r);
+                tom.r    = jerry.r;
+                break;
+            }
+            jerry.r += jerry.v * dt;
+            if (jerry.r.y > 0) {
+                is_safe = true;
+                break;
+            }
+            s_cat   += Norm(tom.v * dt);
+            s_mouse += Norm(jerry.v * dt);
+        }
+        // std::cout << jerry.r << " " << tom.r << std::endl;
+        if (is_catched) {
+            v_res = v_;
             break;
         }
-        jerry.r += jerry.v * dt;
-        if (jerry.r.y > 0) {
-            is_safe = true;
-            break;
-        }
-        s_cat   += getnorm(tom.v * dt);
-        s_mouse += getnorm(jerry.v * dt);
     }
-    std::cout << jerry.r << " " << tom.r << std::endl;
-    if (is_catched) {
-        std::cout << "Jerry is catched! >_<";
-    } else {
-        std::cout << "Jerry is saved!   ^_^";
-    }
-    std::cout << std::endl << s_mouse / s_cat << " " << s_cat / s_mouse;
-
+    std::cout << v_res;
     return 0;
 }
